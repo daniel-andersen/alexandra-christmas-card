60c4614b3c87e818ebc361c9ab4e308324fc1cec
black-knight <dani_ande@yahoo.dk>
2013-11-06 12:31:15 +0100

    Initial Commit

---
+++ MetaioTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MetaioTest
+//
+//  Created by Daniel Andersen on 06/11/13.
+//  Copyright (c) 2013 Alexandra Instituttet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MetaioTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MetaioTest
+//
+//  Created by Daniel Andersen on 06/11/13.
+//  Copyright (c) 2013 Alexandra Instituttet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
